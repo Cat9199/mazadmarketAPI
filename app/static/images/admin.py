9f8d54cea9cd4428752ff0ec1fe42ae45b82e232
Abdelrahman Nasr <pw91994132.office@gmail.com>
2024-04-25 16:11:52 +0200

    faet: dashbored layout

---
+++ app/static/images/admin.py
@@ -0,0 +1,16 @@
+from flask import Blueprint,render_template,session
+
+
+admin_bp = Blueprint('admin', __name__)
+
+@admin_bp.route('/login')
+def admin_login():
+        return render_template('login.html')
+
+@admin_bp.route('/dashboard')
+def admin_dashboard():
+        return render_template('index.html')
+
+@admin_bp.route('/settings')
+def admin_settings():
+        return render_template('index.html')
